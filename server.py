7fc7a948007d727d885728d2315aa4f63da433cd
Ian Mitchell <62729634+ianmitche2@users.noreply.github.com>
2020-05-21 17:34:06 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
-app = Flask(__name__)
-print(__name__)
 
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
@@ -40,7 +43,5 @@ def submit_form():
 		return 'something went wrong. Try again.'
 
 
-if __name__ == '__main__': 
-    app.run()
 
 
